728cce325410c83579d3dabb17fd4240f37e1193
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-01 12:57:47 +0300

    Initial Commit

---
+++ CustomInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomInput
+//
+//  Created by Evgeniy Ryshkov on 01/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
